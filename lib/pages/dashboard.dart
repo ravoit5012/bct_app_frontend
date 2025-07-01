170b5c65ce994cd773c95e4e3b9691e3c8864de5
reddragonnm <neeleshojmishra@gmail.com>
2025-07-01 11:03:45 +0530

    Refactor dashboard navigation to use CurvedNavigationBar and update dependencies

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -2,7 +2,11 @@ import 'package:bct/pages/awareness_page.dart';
 import 'package:bct/pages/camp_page.dart';
 import 'package:bct/pages/home_page.dart';
 import 'package:bct/pages/volunteers_page.dart';
+
 import 'package:flutter/material.dart';
+
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -34,30 +38,46 @@ class _DashBoardState extends State<DashBoard> {
 
       body: _pages[_currentIndex][0],
 
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.red,
+
+        index: _currentIndex,
+
+        buttonBackgroundColor: Colors.redAccent,
+        animationCurve: Curves.fastEaseInToSlowEaseOut,
+
+        items: const [
+          CurvedNavigationBarItem(
+            child: Icon(Icons.home, color: Colors.white),
+            label: 'Home',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.bloodtype, color: Colors.white),
+            label: 'Camp',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.campaign, color: Colors.white),
+            label: 'Awareness',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.person, color: Colors.white),
+            label: 'Volunteers',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+
+          // Icon(Icons.bloodtype, color: Colors.white),
+          // Icon(Icons.campaign, color: Colors.white),
+          // Icon(Icons.person, color: Colors.white),
+        ],
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-
-        type: BottomNavigationBarType.fixed,
-
-        selectedItemColor: Colors.red,
-
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.bloodtype), label: 'Camp'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.campaign),
-            label: 'Awareness',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Volunteers',
-          ),
-        ],
       ),
     );
   }
